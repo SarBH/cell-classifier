f34ae938590efb17fae3e13b149f5bd3c5ca37d0
SarBH <saritahedaya@gmail.com>
2019-06-17 10:39:17 -0400

    going back to rgb

--- train_inception.py
+++ train_inception.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import OneHotEncoder
+# from sklearn.model_selection import train_test_split
 from keras import applications
 from keras.models import Model
 from keras.layers import GlobalAveragePooling2D, Dropout, Dense
@@ -23,19 +23,20 @@ def load_data(dir, max_examples, resize_height, resize_width, label, m_idx):
         if idx == max_examples: # only read max_examples number of examples
             break
         print("Reading " + label + " " + str(idx))
-        img = cv2.imread(dir + "/" + i,0)
+        img = cv2.imread(dir + "/" + i)
         img = cv2.resize(img, (resize_height, resize_width))
         
         # append data and labels to lists
         # assert(img[:,:,0].all() == img[:,:,1].all() == img[:,:,2].all()) # assert image is grayscale and removing dimension wont affect training
         # img = img[:,:,0] # remove the RGB color dimension (no new data)
-        assert(data[m_idx,:,:].shape == img.transpose().shape) # asserts image matrix is of the intended shape, and fits data matrix
-        data[m_idx,:,:] = img.transpose() #add the array to data
+        assert(data[m_idx,:,:,:].shape == img.shape) # asserts image matrix is of the intended shape, and fits data matrix
+        data[m_idx,:,:,:] = img.transpose() #add the array to data
         labels.append(label)
         m_idx = m_idx + 1
     
     # assert all labels went into the lists
     assert(len(labels) == m_idx)
+
 
     return (data, labels, m_idx)
 
@@ -62,6 +63,7 @@ def standardize_data(data):
     """
     x_train = np.array(data, dtype = "float") / 255.0
     print(x_train.shape)
+    # x_train = x_train.reshape(2000,300,300,3)
     return x_train
 
 
@@ -77,7 +79,7 @@ def train_model(x_train, y_train, with_validation=False):
         x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size = 0.1, random_state = 25)
 
     base_model = applications.inception_v3.InceptionV3(weights = None, include_top = False,
-                                                input_shape = (500, 500, 3))
+                                                input_shape = (300, 300, 3))
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     x = Dropout(0.7)(x)
@@ -100,31 +102,37 @@ def train_model(x_train, y_train, with_validation=False):
 
 if __name__ == "__main__":
     curr_m_idx = 0
-    data = np.empty((20, 500, 500), dtype=np.uint8)
+    data = np.empty((2000, 300, 300, 3), dtype=np.uint8)
+    print(sys.getsizeof(data))
     labels = []
 
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/Original', 500, 500, 500, 'viable', curr_m_idx)
+    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/Original', 500, 300, 300, 'viable', curr_m_idx)
     curr_m_idx = m_idx
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/rotate_90', 500, 500, 500, 'viable',curr_m_idx)
+    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/rotate_90', 500, 300, 300, 'viable',curr_m_idx)
     curr_m_idx = m_idx
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/rotate_180', 500, 500, 500, 'viable',curr_m_idx)
-    curr_m_idx = m_idx
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/rotate_270', 500, 500, 500, 'viable',curr_m_idx)
-    print("data has this many bytes: ", sys.getsizeof(data))
-    print(len(data))
+    # data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/rotate_180', 500, 500, 500, 'viable',curr_m_idx)
+    # curr_m_idx = m_idx
+    # data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Colonies_Viable/rotate_270', 500, 500, 500, 'viable',curr_m_idx)
+    # curr_m_idx = m_idx
+    # print("data has this many bytes: ", sys.getsizeof(data))
+    # print(len(data))
 
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/Original', 500, 500, 500, 'unviable',curr_m_idx)
+    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/Original', 500, 300, 300, 'unviable',curr_m_idx)
     curr_m_idx = m_idx
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/rotate_90', 500, 500, 500, 'unviable',curr_m_idx)
+    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/rotate_90', 500, 300, 300, 'unviable',curr_m_idx)
     curr_m_idx = m_idx
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/rotate_180', 500, 500, 500, 'unviable',curr_m_idx)
-    curr_m_idx = m_idx    
-    data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/rotate_270', 500, 500, 500, 'unviable',curr_m_idx)
-    print("data has this many bytes: ", sys.getsizeof(data))
-    print(len(data))
+    # data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/rotate_180', 500, 500, 500, 'unviable',curr_m_idx)
+    # curr_m_idx = m_idx    
+    # data, labels, m_idx = load_data('/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/Diff_And_Bad_Colonies/rotate_270', 500, 500, 500, 'unviable',curr_m_idx)
+    # print("data has this many bytes: ", sys.getsizeof(data))
+    # print(len(data))
     
+    # data = np.load("/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/x_train_2000.npy")
+
     y_train = encode_labels(labels, 'viable', 'unviable')
     x_train = standardize_data(data)
+
+    # np.save("/home/nyscf/Desktop/Classification_Model/Initial_Training_Set/train/x_train_2000.npy", x_train)
 
     train_model(x_train, y_train)
 
