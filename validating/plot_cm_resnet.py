bd0bd973193064bf58f302f12eff14df250897d1
SarBH <saritahedaya@gmail.com>
2019-07-16 12:40:11 -0400

    added support for 4 class and cleaned up

--- validating/plot_cm_resnet.py
+++ validating/plot_cm_resnet.py
@@ -48,88 +48,59 @@ def plot_confusion_matrix(cm, classes,
 
 
 if __name__ == "__main__":
-	""" The file can run on two modes: 
-	    1. Validation: imports labeled examples, and runs a validation through the model. 
-						Prints the accuracy. Creates a confusion matrix
-	    2. Prediction: imports un-labeled examples, and runs predictions using the model. 
-						Prints the predicted label
-	"""
+    """ The file can run on two modes: 
+        1. Validation: imports labeled examples, and runs a validation through the model. 
+                        Prints the accuracy. Creates a confusion matrix
+        2. Prediction: imports un-labeled examples, and runs predictions using the model. 
+                        Prints the predicted label
+    """
 
-	model_path = "/home/nyscf/Documents/sarita/models/resnet_builder/2019-07-12/chkpt_model.53-acc0.94.hdf5"
-	validation_data_dir = "/home/nyscf/Desktop/Classification_Model/data/validation_from_datagen/batch3"
-	img_size = (400,400)
-	num_examples_to_load = 892
-	valid_batch_size = 1
-	num_classes = 3
+    model_path = "/home/nyscf/Documents/sarita/models/resnet_builder/2019-07-15/chkpt_model.40-acc0.95.hdf5"
+    validation_data_dir = "/home/nyscf/Dropbox (NYSCF)/The Array-Pharma Dropbox/Production Projects/ImageAnalysisForMachlineLearning/Training Data/validation_categorization_1"
+    img_size = (400,400)
+    num_examples_to_load = 163
+    valid_batch_size = 1
+    num_classes = 4
 
-	validation_datagen = ImageDataGenerator(rescale=1./255)
-	validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
-													target_size=(img_size[0], img_size[1]),
-													batch_size=valid_batch_size,
-													class_mode=None, 
-													shuffle = False)
-	print("datagen created")
-
-	# base_model = applications.resnet50.ResNet50(include_top=False, weights=None, 
-    #                                             input_shape = (img_size[0], img_size[1], 3), classes=num_classes)
-	# base_model = applications.inception_v3.InceptionV3(weights = None, include_top = False,
-    #                                             input_shape = (img_size[0], img_size[1], 3), classes=num_classes)
-	# base_model.add(GlobalAveragePooling2D())
-	# base_model.add(Dense(num_classes, activation = 'softmax'))
-	
-	# x = base_model.output
-	# x = GlobalAveragePooling2D()(x)
-	# # x = Dense(1024, activation = 'relu')
-	# predictions = Dense(num_classes, activation = 'softmax')(x)
-	# model = Model(inputs=base_model.input, outputs=base_model.output)
-
-	model = load_model(model_path)
-	# model = Sequential()
-	# model.add(prev_model)
-	# # model.add(Dense(256, activation = 'relu'))
-	# # model.add(Dropout(0.5))
-	# model.add(Dense(num_classes, activation = 'softmax'))
-
-	# top_model = Sequential()
-	# top_model.add(Dense(num_classes, activation='softmax')) #sigmoid for resnet
-	# output_layer = base_model.output
-	# output_layer = GlobalAveragePooling2D()(output_layer)
-	# top_model.add(output_layer)
-	# model = Model(inputs=base_model.input, outputs=top_model(output_layer))
-
-	# model.compile(loss = "categorical_crossentropy", optimizer = 'adam', metrics = ["accuracy"]) 
-	#used accuracy metric for 2 classes, supposedly catetgorical acc gets selected automatically when using cat cross ent
+    validation_datagen = ImageDataGenerator(rescale=1./255)
+    validation_generator = validation_datagen.flow_from_directory(validation_data_dir,
+                                                    target_size=(img_size[0], img_size[1]),
+                                                    batch_size=valid_batch_size,
+                                                    class_mode=None, 
+                                                    shuffle = False)
+    print("datagen created")
 
 
-	# model.load_weights(model_path)
 
-	
-	# model = load_model("/home/nyscf/Documents/sarita/cell-classifier/inception/model_inception_m6662_e18_acc93_06_02.h5")
-	
-	# Predict and store predicted labels in y_pred
-	prob = model.predict_generator(validation_generator, steps=num_examples_to_load, verbose=1)
-	print(prob)
-	y_pred = np.argmax(prob, axis=1)
-	print("y_pred: ", y_pred)
-	print("the number of examples predicted are:", y_pred.shape)
-	# Collect y_true
-	y_true = validation_generator.classes
-	print("y_true: ", y_true)
+    model = load_model(model_path)
 
-	if num_classes == 2:
-		target_names = ['bad', 'good']
-	if num_classes == 3:
-		target_names = ['dead','diff', 'viable']
-	print(classification_report(y_true, y_pred, target_names=target_names))
-	cm = confusion_matrix(y_true, y_pred)
-	print(cm)
+    
+    # Predict and store predicted labels in y_pred
+    prob = model.predict_generator(validation_generator, steps=num_examples_to_load, verbose=1)
+    print(prob)
+    y_pred = np.argmax(prob, axis=1)
+    print("y_pred: ", y_pred)
+    print("the number of examples predicted are:", y_pred.shape)
+    # Collect y_true
+    y_true = validation_generator.classes
+    print("y_true: ", y_true)
+
+    if num_classes == 2:
+        target_names = ['bad', 'good']
+    if num_classes == 3:
+        target_names = ['dead','diff', 'viable']
+    if num_classes == 4:
+        target_names = ['M1 ideal colony', 'M2 dark center maybe surrounded by spindly', 'M3 just spindly visually faint', 'M4 dead cells']
+    print(classification_report(y_true, y_pred, target_names=target_names))
+    cm = confusion_matrix(y_true, y_pred)
+    print(cm)
 
 
-	# Plot non-normalized confusion matrix
-	plot_confusion_matrix(cm, classes=target_names,
-						title='Confusion matrix, without normalization')
-	plot_confusion_matrix(cm, classes=target_names, 
-						normalize=True, title="Normalized Confusion matrix")
+    # Plot non-normalized confusion matrix
+    plot_confusion_matrix(cm, classes=target_names,
+                        title='Confusion matrix, without normalization')
+    # plot_confusion_matrix(cm, classes=target_names, 
+    #                     normalize=True, title="Normalized Confusion matrix")
 
-	plt.show()
-		
+    plt.show()
+        
